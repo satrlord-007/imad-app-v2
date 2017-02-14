274191a93876d874f0804fd4cf9dba94b12a8da7
Vikas Shah <vikasshah1996@gmail.com>
2017-02-14 19:31:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/test.html', function (req, res) {
+app.get('/test', function (req, res) {
   res.sendFile(path.join(__dirname, 'vikas', 'test.html'));
 });
 
