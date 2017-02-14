14d1e6fe39d6f4755f87e653c6d94ef814e1b001
Vikas Shah <vikasshah1996@gmail.com>
2017-02-14 19:19:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ app.get('/xyz.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'xyz.html'));
 });
 
-app.get('/ui/style.css', function (req, res) {
+app.get('/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
